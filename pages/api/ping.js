23dbd86c3d4357a6df6a04b895b8e6bb4b55e39d
SmartNetworkSolutions24 <rosorio@smartisvoip.com>
2025-08-15 13:42:08 -0500

    Update ping.js

--- pages/api/ping.js
+++ pages/api/ping.js
@@ -1,5 +1,6 @@
+// pages/api/sms/incoming.js
 export default function handler(req, res) {
-  if (req.method !== 'POST') return res.status(200).end(); // healthcheck
+  if (req.method !== 'POST') return res.status(200).end();
   res.setHeader('Content-Type', 'text/xml');
-  res.status(200).send('<Response/>'); // TwiML válido
+  res.status(200).send('<Response/>');
 }
