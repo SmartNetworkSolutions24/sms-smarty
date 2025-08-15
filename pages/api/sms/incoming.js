d14f479165d158fe1b0830dbfdd0d1a1fcc86172
SmartNetworkSolutions24 <rosorio@smartisvoip.com>
2025-08-15 16:46:56 -0500

    Update incoming.js

--- pages/api/sms/incoming.js
+++ pages/api/sms/incoming.js
@@ -1,4 +1,12 @@
-export default function handler(_req, res) {
-  return res.status(200).send('OK');
-}
+// pages/api/sms/incoming.js
+export default function handler(req, res) {
+  if (req.method !== 'POST') {
+    // Para GET u otros, 200 vacío para no generar alertas.
+    return res.status(200).end();
+  }
 
+  // Twilio quiere XML (TwiML)
+  res.setHeader('Content-Type', 'application/xml');
+  // Respuesta mínima válida para eliminar el 12200
+  res.status(200).send('<?xml version="1.0" encoding="UTF-8"?><Response/>');
+}
