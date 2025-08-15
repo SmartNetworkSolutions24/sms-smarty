79a5cd39d5fb052de9650e1c2a8deb3cd88ca2b4
SmartNetworkSolutions24 <rosorio@smartisvoip.com>
2025-08-15 17:25:25 -0500

    Update incoming.js

--- pages/api/sms/incoming.js
+++ pages/api/sms/incoming.js
@@ -1,4 +1,8 @@
 export default function handler(req, res) {
-  res.setHeader('Content-Type', 'text/xml');
+  // Twilio SIEMPRE usa POST para webhooks de SMS
+  if (req.method !== 'POST') {
+    return res.status(200).end(); // 200 vacío para GET/otros
+  }
+  res.setHeader('Content-Type', 'application/xml');
   return res.status(200).send('<?xml version="1.0" encoding="UTF-8"?><Response></Response>');
 }
