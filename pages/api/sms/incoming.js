88bec2de615467d49ce5a3d5114545f395123835
SmartNetworkSolutions24 <rosorio@smartisvoip.com>
2025-08-15 13:24:16 -0500

    Update incoming.js

--- pages/api/sms/incoming.js
+++ pages/api/sms/incoming.js
@@ -1,4 +1,8 @@
 export default function handler(req, res) {
-  res.status(200).end(); // cuerpo vacío => Content-Length: 0
+  // Twilio publicará por POST; si quieres aceptar GET para healthcheck, puedes
+  if (req.method !== 'POST') {
+    return res.status(200).end(); // healthcheck ok, cuerpo vacío
+  }
+  res.setHeader('Content-Type', 'text/xml');
+  res.status(200).send('<Response/>'); // TwiML válido -> Twilio feliz, sin 12200
 }
-
