c19bc1a9edbfd4715337e9dcb6ac7b531bc7510c
SmartNetworkSolutions24 <rosorio@smartisvoip.com>
2025-08-15 15:08:47 -0500

    Update ping.js

--- pages/api/ping.js
+++ pages/api/ping.js
@@ -2,6 +2,15 @@ export default function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(200).end();
   }
+
+  const { From, To, Body } = req.body;
+
+  // Mostrar en consola
+  console.log('Mensaje recibido:');
+  console.log(`De: ${From}`);
+  console.log(`Para: ${To}`);
+  console.log(`Mensaje: ${Body}`);
+
   res.setHeader('Content-Type', 'application/xml');
-  res.status(200).send('<?xml version="1.0" encoding="UTF-8"?><Response/>');
+  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?><Response/>`);
 }
